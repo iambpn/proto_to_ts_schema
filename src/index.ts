bdd6b76a1cc508abb61f1c4639d8770718f1136c
Bipin Maharjan <bopon.maharjan@gmail.com>
2023-01-19 22:12:44 +0545

    proto parser completed

--- src/index.ts
+++ src/index.ts
@@ -30,3 +30,29 @@ import { ParseProtoFile, ParseProtoLine, printGlobalVars } from "./protoParser";
   }
   printGlobalVars();
 })();
+
+const Types: Record<string, string> = {
+  double: "number",
+  float: "number",
+  int32: "number",
+  int64: "number",
+  uint32: "number",
+  uint64: "number",
+  sint32: "number",
+  sint64: "number",
+  fixed32: "number",
+  fixed64: "number",
+  sfixed32: "number",
+  sfixed64: "number",
+  bool: "boolean",
+  string: "string",
+  bytes: "string",
+};
+
+function convertProtoTypeToTs(p: string) {
+  if (Types[p]) {
+    return Types[p];
+  }
+
+  return p;
+}
--- src/protoParser.ts
+++ src/protoParser.ts
@@ -4,9 +4,10 @@ const MessageHeaderTypes = {
 } as const;
 
 type Import = {
-  packageName: string;
-  path: string;
-  objs: string[];
+  packageName: string; // Name of file to import from
+  pathToPackage: string; // Path to import file (fileName not included)
+  fullPath: string; // Full proto import
+  objs: string[]; // Objects to import from PackageName file
 };
 
 type Option = {
@@ -28,7 +29,10 @@ type MessageHeader = {
 
 type MessageBody = {
   name: string;
-  datatype: string;
+  datatype: {
+    packageName?: string; // File Name to import from. (if empty then import form current file)
+    type: string; // Datatype or Object name
+  };
   repeated: boolean;
   optional: boolean;
   indent?: Indent;
@@ -36,14 +40,22 @@ type MessageBody = {
 
 type EnumBody = {
   name: string;
-  position: number;
+  value: number;
   indent?: Indent;
 };
 
+type AllMessage = MessageHeader | MessageBody | EnumBody;
+
 type RPCFunction = {
   name: string;
-  arg: string;
-  return: string;
+  arg: {
+    packageName?: string;
+    type: string;
+  };
+  returns: {
+    packageName?: string;
+    type: string;
+  };
 };
 
 type Service = {
@@ -55,8 +67,6 @@ type Syntax = string;
 
 type Package = string;
 
-type Nested = boolean;
-
 type Line = {
   import?: Import;
   option?: Option;
@@ -64,32 +74,6 @@ type Line = {
   syntax?: Syntax;
   package?: Package;
 };
-
-const Types: Record<string, string> = {
-  double: "number",
-  float: "number",
-  int32: "number",
-  int64: "number",
-  uint32: "number",
-  uint64: "number",
-  sint32: "number",
-  sint64: "number",
-  fixed32: "number",
-  fixed64: "number",
-  sfixed32: "number",
-  sfixed64: "number",
-  bool: "boolean",
-  string: "string",
-  bytes: "string",
-};
-
-function convertProtoTypeToTs(p: string) {
-  if (Types[p]) {
-    return Types[p];
-  }
-
-  return p;
-}
 
 export function ParseProtoFile(file: string): string[] {
   if (!file) {
@@ -125,6 +109,7 @@ const options: Option[] = [];
 const messages: MessageHeader[] = [];
 const services: Service[] = [];
 let nestedLevel = 0;
+let lastMessageType: keyof typeof MessageHeaderTypes | undefined;
 
 export function ParseProtoLine(line: string) {
   const tokens = line
@@ -140,9 +125,14 @@ export function ParseProtoLine(line: string) {
       {
         const commentToken = line.substring(2).split(/:| /).filter(Boolean);
         if (commentToken[0] === "package") {
+          const packageName = commentToken[1];
           let lastImport = Array.from(imports.keys()).pop();
-          if (lastImport && imports.has(lastImport)) {
-            imports.get(lastImport)!.packageName = commentToken[1];
+          const import_data = imports.get(lastImport ?? "");
+
+          if (import_data) {
+            imports.delete(lastImport ?? "");
+            import_data.packageName = packageName;
+            imports.set(packageName, import_data);
           }
         }
       }
@@ -159,11 +149,15 @@ export function ParseProtoLine(line: string) {
       return;
     case "import":
       {
-        const key = tokens[tokens.length - 1];
-        imports.set(key, {
+        const fullPath = tokens[tokens.length - 1];
+        const pathTokens = fullPath.split("/");
+        const packageName = pathTokens[pathTokens.length - 1].split(".")[0];
+
+        imports.set(packageName, {
           objs: [],
-          packageName: key.split(".")[0],
-          path: key,
+          packageName,
+          pathToPackage: pathTokens.length > 1 ? pathTokens.slice(0, -1).join("/") : ".",
+          fullPath,
         });
       }
       return;
@@ -206,6 +200,26 @@ export function ParseProtoLine(line: string) {
       }
       return;
     case "rpc":
+      {
+        const rpcTokens = line.split(/ |\(|\)/gm).filter(Boolean);
+
+        const [argPackageName, argObjName] = getPackageNameAndObjName(rpcTokens[2]);
+        const [returnPackageName, returnObjName] = getPackageNameAndObjName(rpcTokens[4]);
+
+        const rpcFn: RPCFunction = {
+          name: rpcTokens[1],
+          arg: {
+            packageName: argPackageName,
+            type: argObjName,
+          },
+          returns: {
+            packageName: returnPackageName,
+            type: returnObjName,
+          },
+        };
+
+        services[services.length - 1].rpcFunctions.push(rpcFn);
+      }
       return;
     case "}":
       nestedLevel -= 1;
@@ -218,10 +232,40 @@ export function ParseProtoLine(line: string) {
       break;
   }
 
-  
+  const parentMessage = getLastMessageHeader(messages[messages.length - 1], messages[messages.length - 1]);
+
+  let message: MessageBody | EnumBody;
+  if (parentMessage.type === "Message") {
+    const datatype = isRepeated || isOptional ? tokens[1] : tokens[0];
+    const [dataTypePackageName, dataTypeObj] = getPackageNameAndObjName(datatype);
+    message = {
+      datatype: {
+        packageName: dataTypePackageName,
+        type: dataTypeObj,
+      },
+      name: isRepeated || isOptional ? tokens[2] : tokens[1],
+      optional: isOptional,
+      repeated: isRepeated,
+    } as MessageBody;
+  } else {
+    message = {
+      name: tokens[0],
+      value: +tokens[2],
+    } as EnumBody;
+  }
+
+  parentMessage.body.push(message);
 }
 
-function insertMessage(nestedCount: number, message: MessageHeader | MessageBody | EnumBody, insertToNode: (MessageHeader | MessageBody | EnumBody)[]) {
+function getLastMessageHeader(lastMessage: AllMessage, parentMessage: MessageHeader): MessageHeader {
+  if (!lastMessage || !("body" in lastMessage)) {
+    return parentMessage;
+  }
+
+  return getLastMessageHeader(lastMessage.body[lastMessage.body.length - 1], lastMessage);
+}
+
+function insertMessage(nestedCount: number, message: AllMessage, insertToNode: AllMessage[]) {
   if (nestedCount > 0) {
     const lastElement = insertToNode[insertToNode.length - 1] as MessageHeader;
     insertMessage(nestedCount - 1, message, lastElement.body);
@@ -229,6 +273,26 @@ function insertMessage(nestedCount: number, message: MessageHeader | MessageBody
   }
 
   insertToNode.push(message);
+}
+
+function insertImport(package_name: string, obj: string) {
+  const import_data = imports.get(package_name);
+  if (import_data) {
+    const set_data = new Set(import_data.objs);
+    set_data.add(obj);
+    import_data.objs = Array.from(set_data);
+  }
+}
+function getPackageNameAndObjName(messageType: string): [string | undefined, string] {
+  const messageTypeToken = messageType.split(".");
+
+  // For Import Management
+  const packageName = messageTypeToken.length > 1 ? messageTypeToken.slice(0, -1).join(".") : undefined;
+  const objName = messageTypeToken[messageTypeToken.length - 1];
+
+  insertImport(packageName ?? "", objName);
+
+  return [packageName, objName];
 }
 
 function cleanText(text: string) {
