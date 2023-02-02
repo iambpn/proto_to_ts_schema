9b04f0c81cd70f1016b8909f6ab0203ff7e54083
Bipin Maharjan <bopon.maharjan@gmail.com>
2023-02-02 17:42:42 +0545

    updating example test file

--- Example/test.ts
+++ Example/test.ts
@@ -1,15 +1,46 @@
-import { z } from "zod";
+import { Observable } from 'rxjs';
+import { Timestamp } from "google/protobuf/google.protobuf";
+import { common } from "./commons";
 
-const email = z.string().email();
-
-const test = z.object({
-  email: email,
-});
-
-const test2 = z.object({
-  t: test,
-});
-
-interface TestService {
-  testfn(data: z.infer<typeof test>): any;
+enum Person__PhoneType {
+  MOBILE,
+  HOME,
+  WORK,
+}
+export interface Person__PhoneNumber {
+  number: string;
+  type: Person__PhoneType;
+}
+export interface Person {
+  name: string;
+  id: number;
+  email: string;
+  password: string;
+  phones: Person__PhoneNumber[];
+  last_updated: Timestamp;
+}
+export interface AddressBook__AddressBook1__AddressBook2__AddressBook3__PhoneNumber {
+}
+export interface AddressBook__AddressBook1__AddressBook2__AddressBook3 {
+}
+export interface AddressBook__AddressBook1__AddressBook2 {
+}
+export interface AddressBook__AddressBook1 {
+}
+export interface AddressBook {
+  people: Person[];
+  address: AddressBook__AddressBook1__AddressBook2__AddressBook3;
+}
+export interface UpdateCallLogsRequest__UpateFlags {
+  add: string;
+  delete: string;
+  set: string;
+}
+export interface UpdateCallLogsRequest {
+  call_link_id: string;
+  flags: UpdateCallLogsRequest__UpateFlags;
+}
+export interface tutorialService {
+  getData(data:AddressBook):Observable<common>;
+  setdata(data:AddressBook):Observable<common>;
 }
